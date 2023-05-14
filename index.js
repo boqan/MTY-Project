e3ae1a06daac6ba4b3568fdb866f08d5ec30f167
Boyan Atanasov <bojan.atanasov2002@gmail.com>
2023-05-14 14:52:24 +0300

    Wrote basic logic to interpret the txt file to a json object array

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+const fs = require("fs");
+const readline = require("readline");
+
+let users = [];
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream("partners.txt"),
+  output: process.stdout,
+  console: false,
+});
+
+readInterface.on("line", function (line) {
+  users.push(JSON.parse(line));
+});
+
+readInterface.on("close", function () {
+  users.forEach((user) => {
+    console.log(user);
+  });
+});
